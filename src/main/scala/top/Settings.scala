33a99fe6a4b880d7a6a043d8745dd782c2a7986a
xuyibin <xuyibin17@mails.ucas.ac.cn>
2021-08-24 17:20:33 +0800

    Implement registers of RISC-V N extension, and support three DASICS user exceptions

--- src/main/scala/top/Settings.scala
+++ src/main/scala/top/Settings.scala
@@ -39,7 +39,8 @@ object DefaultSettings {
     "FPGAPlatform" -> false,
     "EnableILA" -> true,
     "EnableDebug" -> true,
-    "EnableRVC" -> true
+    "EnableRVC" -> true,
+    "EnableRVN" -> true
   )
 }
 
