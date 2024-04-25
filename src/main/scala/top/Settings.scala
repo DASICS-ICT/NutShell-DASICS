7238832a7c31f1bcca1721d8491fd01075b89bf0
Gwins7 <gwins777@qq.com>
2024-04-26 01:01:05 +0800

    only open PLPeriphery in dual setting

--- src/main/scala/top/Settings.scala
+++ src/main/scala/top/Settings.scala
@@ -49,7 +49,6 @@ object DefaultSettings {
 object PynqSettings {
   def apply() = Map(
     "FPGAPlatform" -> true,
-    "PLPeriphery" -> true,
     "NrExtIntr" -> 3,
     "ResetVector" -> 0x50000000L,
     "MemMapBase" -> 0x0000000010000000L,
@@ -101,7 +100,8 @@ object EmbededSettings {
 object DualSettings {
   def apply(): Map[String, Boolean] = Map(
     "HasPrefetch" -> false,
-    "HasDualCore" -> true
+    "HasDualCore" -> true,
+    "PLPeriphery" -> true
   )
 
   def oslab(): Map[String, Boolean] = apply() ++ Map(
